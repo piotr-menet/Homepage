4d2c2a13c94f525dc0bd4e14707e82151e27cd9e
Piotr Menet <piotr.menet@gmail.com>
2023-05-04 21:26:27 +0100

    Changing js to have all code as a function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,50 @@
 
-let button = document.querySelector(".js__button");
-let body = document.querySelector(".js__body");
+// const button = document.querySelector(".js__button");
+// const body = document.querySelector(".js__body");
 
-button.addEventListener("click", () => {
+
+
+
+// button.addEventListener("click", () => {
+//     body.classList.toggle("js__body--dark");
+
+//     if (body.classList.contains("js__bodyDark")) {
+//         button.innerText = "Wlacz jasny motyw";
+//     } else {
+//         button.innerText = "Wlacz ciemny motyw";
+//     }
+// });
+
+
+function getElements() {
+    const button = document.querySelector(".js__button");
+    const body = document.querySelector(".js__body");
+    return { button, body };
+  }
+  
+  function toggleDarkMode(body) {
     body.classList.toggle("js__body--dark");
-
-    if (body.classList.contains("js__bodyDark")) {
-        button.innerText = "Wlacz jasny motyw";
+  }
+  
+  function updateButtonText(body, button) {
+    if (body.classList.contains("js__body--dark")) {
+      button.innerText = "Wlacz jasny motyw";
     } else {
-        button.innerText = "Wlacz ciemny motyw";
+      button.innerText = "Wlacz ciemny motyw";
     }
-});
-
-
+  }
+  
+  function addButtonEventListener(button, body) {
+    button.addEventListener("click", () => {
+      toggleDarkMode(body);
+      updateButtonText(body, button);
+    });
+  }
+  
+  function init() {
+    const { button, body } = getElements();
+    addButtonEventListener(button, body);
+  }
+  
+  init();
+  
